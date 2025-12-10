5674f817ce40f88900884807ee6e85f6f2cd0c1f
GeoffroyLoiselet <geoffroy.loiselet@ensea.fr>
2025-12-10 16:21:16 +0100

    “deuxieme”

--- Question1.c
+++ Question1.c
@@ -1,35 +1,63 @@
-#include <stdio.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
-void commande(char *entree) {
-	entree[strcspn(entree, "\n")] = '\0';
-	if (strcmp(entree, "test") == 0) {
-		write(1, "Ceci est un test.\r\n", 20);
-	}
-	else if(strcmp(entree, "exit") == 0) {
-		write(1, "Fermeture de l'EnseaSH.\r\n", 25);
-		exit(0);
-	}
-	else {
-		write(1, "Commande inconnue.\r\n", 21);
-	}
+#define MSG_Welcome "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n"
+#define MSG_Prompt "enseash % "
+#define MSG_Exit "Fin du programme\n"
+#define MSG_ForkError "Erreur de fork\n"
+#define MSG_InvalidCommand "La commande saisie est introuvable \n"
+#define PROMPT_LEN strlen(PROMPT)
+#define BUFFER_SIZE 128
+
+char buffer[BUFFER_SIZE]; // Command typed by the user
+ssize_t command; // Length of the Command
+
+// Function that prints in the terminal
+void print(const char* msg) {
+    write(STDOUT_FILENO, msg, strlen(msg));
 }
 
-int main (){
-	char buf[100];
-	write(1,"Bienvenue dans le Shell ENSEA.\r\n", 33);
-	write(1,"Pour quitter, tapez 'exit'.\r\n", 29);
-	
-	while(1){
-		write(1,"enseash % ", 10);
-		int n = read(0, buf, sizeof(buf)-1);
-		buf[n] = '\0';
-		write(1,buf, n);
-		commande(buf);
-	}
-
-return 0;
+// Function that reads the command typed in the terminal and formats it
+void readCommand(char* buffer, ssize_t* cmd) {
+    *cmd = read(STDIN_FILENO, buffer, BUFFER_SIZE - 1);
+    buffer[command] = '\0';
+    if (buffer[command - 1] == '\n') {
+        buffer[command - 1] = '\0';
+    }
+    printf("Command typed : %s\n", buffer);
 }
+
+int main() {
+    print(MSG_Welcome);
+
+    while (1) {
+        print(MSG_Prompt);
+        readCommand(buffer, &command);
+
+        if (strcmp(buffer, "exit") == 0) {
+            print(MSG_Exit);
+            break;
+        }
+
+        pid_t pid = fork();
+        pid_t status;
+
+        if (pid < 0) {
+            print(MSG_ForkError);
+        } else if (pid > 0) {
+            wait(&status);
+        } else {
+            char *args[] = {buffer, NULL};
+            execvp(buffer, args);
+            print(MSG_InvalidCommand);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return 0;
+}
+
